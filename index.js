6b55d1687667332f366b8962b46b0ba260ba9f9c
priya mina <103628960+priyamina@users.noreply.github.com>
2023-06-26 02:13:45 +0530

    Update index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+var hits = 0;
+var hitElement = document.querySelector( '.hits' );
+document.body.onkeyup = function(a) {
+if( a.keyCode == 32 ) {
+addHit();
+}
+
+// Submitted by:PRIYA MINA ,ECE 2ND YEAR,priya040btece21@igdtuw.ac.in
+}
+var addHit = function() {
+hits++;
+renderHits();
+}
+var renderHits = function() {
+hitElement.innerHTML = hits;
+}
+var resetHits = function() {
+hits = 0;
+renderHits();
+}
